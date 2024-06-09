64fb541443974ef82eb14db93ecb5cde9eec3ec8
sujitbarnawal <sujitbarnawal4545@gmail.com>
2024-06-09 13:37:29 +0545

    js file

--- Project 1-Weather_app/script.js
+++ Project 1-Weather_app/script.js
@@ -1,6 +1,30 @@
-let apiKey="5548afa741f2ca138592be96c1fa8c82"
-let apiURL=`https://api.openweathermap.org/data/2.5/weather?units=metric&q=hetauda&appid=${apiKey}`
+let humidity=document.querySelector(".humidity")
+let wind=document.querySelector(".wind")
+let temp=document.querySelector(".temp")
+let city=document.querySelector(".city")
+let search_input=document.querySelector(".search input")
+let search_button=document.querySelector(".search button")
 
-async function checkWeather(){
-    let response=await fetch(apiURL)
+
+let apiKey="5548afa741f2ca138592be96c1fa8c82"
+let apiURL="https://api.openweathermap.org/data/2.5/weather?units=metric&q="
+
+async function checkWeather(cityName){
+    
+    let response=await fetch(apiURL+cityName+`&appid=${apiKey}`)
+    let data= await response.json()
+
+    city.innerText=`${data.name}`
+    humidity.innerText=`${data.main.humidity}%`
+    wind.innerText=`${data.wind.speed}Km/hr`
+    temp.innerText=`${Math.round(data.main.temp)}°C`
+
+
+
+    // console.log(data)
+    // console.log(data.main.humidity)
 }
+
+search_button.addEventListener("click",()=>{
+    checkWeather(search_input.value)
+})
